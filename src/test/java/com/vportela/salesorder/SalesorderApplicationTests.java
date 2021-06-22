fbb13edb86b38c0e03635893276e33feb419338d
Victor Portela <portelavictor@hotmail.com>
2021-06-22 09:52:39 -0300

    Project created

---
+++ src/test/java/com/vportela/salesorder/SalesorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vportela.salesorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
